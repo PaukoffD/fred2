64e4e3b9487cb82e647d61e99688a5017de113a2
xuliaP <paukov2967@gmail.com>
2016-08-18 15:42:50 +0300

    'avatar'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817113132) do
+ActiveRecord::Schema.define(version: 20160818120032) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",           limit: 191, null: false
@@ -253,6 +253,10 @@ ActiveRecord::Schema.define(version: 20160817113132) do
     t.datetime "updated_at",                          null: false
     t.boolean  "admin"
     t.string   "name"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
